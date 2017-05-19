04444e0626c3f3530a14448ead9a53cbe1e59b20
Yutaro Hayakawa <river@shinano3.ht.sfc.keio.ac.jp>
2017-05-19 21:52:59 +0900

    Added copyright

--- sys/vale_bpf_loader.c
+++ sys/vale_bpf_loader.c
@@ -1,6 +1,8 @@
 /*
  * Copyright 2015 Big Switch Networks, Inc
  *
+ * Modified by Yutaro Hayakawa in 2017
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
